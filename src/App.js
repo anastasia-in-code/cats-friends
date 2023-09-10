ea7292f532324ab99c6cd3c58fb3068bda99a1ba
Anastasiia Perih <anastasiia.perih@gmail.com>
2023-09-10 20:08:46 +0200

    api added

--- src/App.js
+++ src/App.js
@@ -13,14 +13,23 @@ class App extends Component {
       }
    }
 
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+         .then(res => res.json())
+         .then(users => this.setState({ cats: users }))
+   }
+
    onSearchChange = (event) => {
-      this.setState({searchValue : event.target.value})
+      this.setState({ searchValue: event.target.value })
    }
 
    render() {
       const filteredCats = this.state.cats.filter(cat => {
          return cat.name.toLowerCase().includes(this.state.searchValue.toLowerCase())
       })
+      if (!this.state.cats.length) {
+         return <h1 className='tc'>Loading...</h1>
+      }
       return <div className='tc'>
          <h1 className='f2'>CATS FROM CARTOONS</h1>
          <SearchBox searchValue={this.state.searchValue} onSearchChange={this.onSearchChange} />
